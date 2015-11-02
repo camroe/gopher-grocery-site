ddc60816bdfac7d61be5d8fa5f290077798997ee
camroe@gmail.com <camroe@gmail.com>
2015-11-02 00:22:25 -0700

    Works for deploying to the web server \n Json return from category service returns correct json

--- src/test/java/com/gophergroceries/model/repository/ProductRepositoryTest.java
+++ src/test/java/com/gophergroceries/model/repository/ProductRepositoryTest.java
@@ -4,7 +4,6 @@ package com.gophergroceries.model.repository;
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
