9170f5badc4f3725a6ec5d408a58d9c107a71b60
camroe <camroe@gmail.com>
2015-11-12 09:08:24 -0700

    Commented out SecurityConfiguration - Web/Url based security and moved code to MethodSecurityConfiguration

--- src/main/java/com/gophergroceries/config/MethodSecurityConfiguration.java
+++ src/main/java/com/gophergroceries/config/MethodSecurityConfiguration.java
@@ -1,10 +1,25 @@
 package com.gophergroceries.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+	@Autowired
+	@Qualifier("gophergroceriesMySQL")
+	DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
 }
--- src/main/java/com/gophergroceries/config/SecurityConfiguration.java
+++ src/main/java/com/gophergroceries/config/SecurityConfiguration.java
@@ -1,28 +1,21 @@
 package com.gophergroceries.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
+// EVERYTHING COMMENTED OUT TO TEST METHOD ONLY SECURITY.
+// @Configuration
+// @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	@Qualifier("gophergroceriesMySQL")
-	DataSource dataSource;
+	// @Autowired
+	// @Qualifier("gophergroceriesMySQL")
+	// DataSource dataSource;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().formLogin().and().authorizeRequests().antMatchers("**/bread")
-				.authenticated().and().httpBasic().and().logout().logoutSuccessUrl("/");
-	}
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// http.csrf().disable().formLogin().and().authorizeRequests().antMatchers("**/bread")
+	// .authenticated().and().httpBasic().and().logout().logoutSuccessUrl("/");
+	// }
 
 	// IN MEMORY FOR TESTING AND DEBUGGING
 	// @Override
@@ -32,9 +25,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// .password("password").roles("USER", "ADMIN");
 	// }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource);
-	}
+	// @Override
+	// protected void configure(AuthenticationManagerBuilder auth) throws
+	// Exception {
+	// auth.jdbcAuthentication().dataSource(dataSource);
+	// }
 
 }
